b0e5f90f13abf3d9fc0db4d42c2bf9a28ad78ae5
Sammwy <sammwy.dev@gmail.com>
2023-04-09 17:39:49 -0300

    fix: compatibility with legacy locale

--- src/main/java/com/sammwy/libi18n/utils/PlayerUtils.java
+++ src/main/java/com/sammwy/libi18n/utils/PlayerUtils.java
@@ -18,12 +18,9 @@ public class PlayerUtils {
     }
 
     public static String getPlayerLocale(Player player) {
-        System.out.println("getPlayerLocale");
         if (ServerUtils.hasPlayerGetLocaleAPI()) {
-            System.out.println("hasPlayerGetLocaleAPI returns true");
             return player.getLocale();
         } else {
-            System.out.println("hasPlayerGetLocaleAPI returns false");
             return getPlayerLocaleInLegacyWay(player);
         }
     }
